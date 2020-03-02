f6848c257c1861d40b975ddf309c5993aee61c5e
TaskRabbit14 <34329413+TaskRabbit14@users.noreply.github.com>
2020-03-02 00:07:08 -0500

    Add files via upload

---
+++ IST412SpringMedApp/IST412SpringMedApp/src/ist412/IST412.java
@@ -0,0 +1,39 @@
+package ist412;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import Tests.Test;
+import Tests.kelliaTest;
+
+/**
+ *
+ * @author Angel Wu
+ */
+public class IST412 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        Test test= new Test();
+        test.testAll();
+        kelliaTest test2 = new kelliaTest();
+        test2.testAll();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
