fd396f4f0431d902d48f73d3baefafa99cc385b9
kelliacockerill <55554898+kelliacockerill@users.noreply.github.com>
2020-03-29 21:51:51 -0400

    Update LoginUIController.java

--- src/Controller/LoginUIController.java
+++ src/Controller/LoginUIController.java
@@ -1,8 +1,15 @@
 package Controller;
 
+import Model.UserList;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -10,7 +17,15 @@ import javafx.fxml.Initializable;
  * @author Angel Wu
  */
 public class LoginUIController implements Initializable {
-
+    @FXML
+    private Label label;
+    @FXML
+    private TextField logInID;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button go;
+    
     /**
      * Initializes the controller class.
      */
@@ -18,7 +33,25 @@ public class LoginUIController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+  
+    @FXML
+    public void goPressed() {
+        System.out.println("Submit!");
+       UserList list = new UserList();
+        if (list.authenticate(logInID, password) == true && logInID.getText().equals("DRtest"))
+            { 
+                
+                label.setText("Valid- Medical Personell.");
+                DoctorHomeUIController newController = new DoctorHomeUIController();
+                newController.setStage((Stage)label.getScene().getWindow());
+               
+            }
+        else 
+            {
+                label.setText("Invalid. Try again.");
+            }
+        
+    }
     /**
      * Checks for username and password verification
      * @return A boolean for the if the password and username match
