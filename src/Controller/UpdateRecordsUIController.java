ec8705b295dcddb74b1d922a15ffc799b19de6c0
kelliacockerill <55554898+kelliacockerill@users.noreply.github.com>
2020-03-29 23:55:11 -0400

    Update UpdateRecordsUIController.java

--- src/Controller/UpdateRecordsUIController.java
+++ src/Controller/UpdateRecordsUIController.java
@@ -1,11 +1,32 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class UpdateRecordsUIController {
     
     /**
      * Creates a MedicalRecord Object and adds it to the patient
      * @param patientID Sets the id to the record and adds it to the patient record
      */
+    public void setStage(Stage stage) 
+    {
+        try
+        {
+        Parent root = FXMLLoader.load(getClass().getResource("../View/UpdateRecordsUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Medical Personell Homescreen");
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(Exception e)
+                {
+                    e.printStackTrace();
+                     System.out.println("Error cannot open Doctor Home UI");
+                } 
+    }
     public void updatePatientRecord(String patientID){
     
     }
