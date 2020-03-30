e7360600fbb87cc755dbcabd35864c20bc9f61f9
kelliacockerill <55554898+kelliacockerill@users.noreply.github.com>
2020-03-29 23:54:22 -0400

    Update SendPrescriptionUIController.java

--- src/Controller/SendPrescriptionUIController.java
+++ src/Controller/SendPrescriptionUIController.java
@@ -1,5 +1,10 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class SendPrescriptionUIController {
     protected String selectedPatientID;
     
@@ -10,7 +15,22 @@ public class SendPrescriptionUIController {
     public void generatePrescription(String selectedPatientID){
         
     }
-    
+    public void setStage(Stage stage) 
+    {
+        try
+        {
+        Parent root = FXMLLoader.load(getClass().getResource("../View/SendPrescriptionUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Medical Personell Homescreen");
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(Exception e)
+                {
+                    e.printStackTrace();
+                     System.out.println("Error cannot open Doctor Home UI");
+                } 
+    }
     /**
      * Sets selectedPatientID to the ID of the patient the Doctor is writing a prescription for
      */
