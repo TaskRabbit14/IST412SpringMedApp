2a3f07ad6d167018c885d25974bcb2f042814cf8
kelliacockerill <55554898+kelliacockerill@users.noreply.github.com>
2020-03-29 23:54:52 -0400

    Update DoctorAccessRecordsUIController.java

--- src/Controller/DoctorAccessRecordsUIController.java
+++ src/Controller/DoctorAccessRecordsUIController.java
@@ -1,11 +1,32 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class DoctorAccessRecordsUIController {
     
     /**
      * Gets the records of the patient and displays them in the UI
      * @param patientID Uses the patientsID to find the records to display
      */
+    public void setStage(Stage stage) 
+    {
+        try
+        {
+        Parent root = FXMLLoader.load(getClass().getResource("../View/DoctorAccessRecordsUI.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Medical Personell Homescreen");
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(Exception e)
+                {
+                    e.printStackTrace();
+                     System.out.println("Error cannot open Doctor Home UI");
+                } 
+    }
     public void displayRecords(String patientID){
     }
 }
