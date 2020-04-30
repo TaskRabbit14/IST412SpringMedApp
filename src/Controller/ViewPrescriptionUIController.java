6b591de4eb577d48a3c8aaa44e3d3ed611180ec7
Angel Wu <44008675+arawup@users.noreply.github.com>
2020-04-30 18:22:06 -0400

    Updated View and edit prescription controller with copy of records

--- src/Controller/ViewPrescriptionUIController.java
+++ src/Controller/ViewPrescriptionUIController.java
@@ -8,7 +8,10 @@ package Controller;
 import Model.Patient;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -29,6 +32,63 @@ public class ViewPrescriptionUIController implements Initializable {
     {
         System.out.println("Made it to view prescription method.");
         
+    }
+    
+  
+    
+    public Patient patient;
+    @FXML private AnchorPane currentRecordPane;
+    @FXML private Label firstName;
+    @FXML private Label lastName;
+    @FXML private Label birthDate;
+    @FXML private Label sex;
+    @FXML private Label bloodType;
+    @FXML private Label patientID;
+    @FXML private Label SSN;
+    
+    public void viewPatientRecord(Patient p)
+    {
+            
+        patient = p;
+        System.out.println("Current Record: " + patient.getFullName());
+        firstName.setText(patient.getFirstName());
+        lastName.setText(patient.getLastName());
+        birthDate.setText(patient.getBirthDate().toString());
+        sex.setText(patient.getSex());
+        bloodType.setText(patient.getBloodType());
+        patientID.setText(patient.getPatientID());
+        SSN.setText(patient.getSSN());
+        
+        
+
+    }
+    
+//    public void setStage(Stage stage) 
+//    {
+//        try
+//        {
+//        Parent root = FXMLLoader.load(getClass().getResource("../View/ViewRecord.fxml"));
+//        Scene scene = new Scene(root);
+//        scene.setRoot(root);
+//        stage.setTitle("View a Patient Record");
+//        stage.setScene(scene);
+//        stage.show();
+//        }
+//        catch(Exception e)
+//                {
+//                    e.printStackTrace();
+//                     System.out.println("Error cannot open View Record UI");
+//                } 
+//    }
+// 
+    
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
     
 }
